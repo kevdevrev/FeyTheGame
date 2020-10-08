85ea8abbc44e4061249d2ac7056e26ec9b4139d7
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2020-10-08 18:56:52 -0400

    glow

---
+++ Assets/Fey/Dissolve.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    private Material material;
+
+    private bool isDissolving = false;
+
+    private float fade = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //isDisolving = true;
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime;
+            //Set the property
+            if (fade <= 0f)
+            {
+                fade = 0f;
+                isDissolving = false;
+            }
+
+            material.SetFloat("_Fade", fade);
+        }
+    }
+}
