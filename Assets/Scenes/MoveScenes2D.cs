95de828fe5aaefcd0d43a2d543c2481528031da8
Ethan <beauprethan@gmail.com>
2020-11-13 23:31:22 -0500

    scene fix

--- Assets/Scenes/MoveScenes2D.cs
+++ Assets/Scenes/MoveScenes2D.cs
@@ -9,7 +9,7 @@ public class MoveScenes2D : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Fey"))
         {
             SceneManager.LoadScene(level);
         }
