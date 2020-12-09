b5a598dbc00033cea0247736042ce0a35c4fb6e3
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2020-12-09 16:31:09 -0500

    Dialouge is here!

---
+++ Assets/UI/Dialogue Box/Dialouge_Triggers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialouge_Triggers : MonoBehaviour
+{
+    [SerializeField] public GameObject target;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Fey"))
+        {
+            Debug.Log("ACTIVATE DIALOG");
+            target.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Fey"))
+        {
+            Debug.Log("DEACTIVATE DIALOG");
+            target.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+}
