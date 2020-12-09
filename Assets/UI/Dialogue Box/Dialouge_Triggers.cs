c86cfaa965bb25821efc1832d8777eca03ac74aa
kevdevrev <44076063+kevdevrev@users.noreply.github.com>
2020-12-09 16:53:26 -0500

    First stage dialouge had

--- Assets/UI/Dialogue Box/Dialouge_Triggers.cs
+++ Assets/UI/Dialogue Box/Dialouge_Triggers.cs
@@ -10,7 +10,7 @@ public class Dialouge_Triggers : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Fey"))
+        if (other.CompareTag("Fey") && other.gameObject.layer == 17)
         {
             Debug.Log("ACTIVATE DIALOG");
             target.gameObject.transform.GetChild(0).gameObject.SetActive(true);
@@ -19,7 +19,7 @@ public class Dialouge_Triggers : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Fey"))
+        if (other.CompareTag("Fey") && other.gameObject.layer == 17)
         {
             Debug.Log("DEACTIVATE DIALOG");
             target.gameObject.transform.GetChild(0).gameObject.SetActive(false);
